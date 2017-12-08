66acec1e7a812757ea284d5310f94b7c0167087e
Arnav Bansal <arnav.bansal95@gmail.com>
2017-12-08 23:53:47 +0530

    Add files via upload

---
+++ Ultrasonic.h
@@ -0,0 +1,48 @@
+/*
+	Ultrasonic (HC-SR04) - Sensing with Advanced Accuracy
+
+	Ultrasonic Waves follow the properties of generic sound waves, hence 
+	there might be a change in speed (microscopic) in either direction with 
+	change in temperature. This change generates error in calculation and 
+	hence different distant values are obtained for the same object kept at 
+	a fixed  distance. To overcome this, a special algorithm is used which 
+	returns the exact distance. 
+	
+	
+	The circuit:
+		* VCC connection of the sensor attached to +5V
+		* GND connection of the sensor attached to ground
+		* TRIG connection of the sensor (User Defined)
+		* ECHO connection of the sensor (User Defined)
+	
+	Created by Arnav Bansal. Last Edited: 08 December, 2017.
+	Github: https://github.com/arnavbansal95/Ultrasonic-Arduino-HC-SR04
+	
+*/
+
+#ifndef Ultrasonic_h
+#define Ultrasonic_h
+
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+class Ultrasonic
+{
+  public:
+    Ultrasonic(int trig, int echo);
+	long getRawData();
+	void getArrayData();
+	void getCountArray();
+	int getMaxElemPos();
+	long readDistance();
+	
+  private:
+    int trig_pin;
+    int echo_pin;
+    long duration, raw_dist;
+};
+
+#endif
---
+++ examples/ButtonSerial/ButtonSerial.ino
@@ -0,0 +1,21 @@
+#include <Ultrasonic.h>
+
+Ultrasonic sensor(12,13); // Ultrasonic _ObjectName_(TRIG, ECHO)
+
+int btn = 2; // Switch -> PIN 2 on Arduino
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(btn, INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(btn)==LOW)
+  {
+    Serial.print(sensor.readDistance());
+    Serial.println(" cm");  
+  }
+  delay(100);
+}
