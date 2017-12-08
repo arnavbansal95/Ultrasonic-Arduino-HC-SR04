7922429546fd1f953b92c9123dd9a1b78debfad2
Arnav Bansal <arnav.bansal95@gmail.com>
2017-12-08 13:43:45 +0530

    Add files via upload

---
+++ Ultrasonic/examples/DistanceSerialTest/DistanceSerialTest.ino
@@ -0,0 +1,17 @@
+#include <Ultrasonic.h>
+
+Ultrasonic sensor(12,13); // Ultrasonic _ObjectName_(TRIG, ECHO)
+long distance;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  distance = sensor.readDistance();
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
+}
